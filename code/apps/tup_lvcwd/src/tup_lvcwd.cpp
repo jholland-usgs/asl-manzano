f70e78480fd282132afaf0e182017c1951d27cd3
rfigueroa <rfigueroa@usgs.gov>
2017-07-13 21:26:48 +0000

    Create and test tup_lvcwd with serial

--- code/apps/tup_lvcwd/src/tup_lvcwd.cpp
+++ code/apps/tup_lvcwd/src/tup_lvcwd.cpp
@@ -4,6 +4,7 @@
 #include <exception>
 #include <cstdlib>
 #include "serial_connection.h"
+#include <thread>
 
 // -------------------------------------------------------------------------- //
 int main(int argc, char **argv) {
@@ -12,12 +13,12 @@ int main(int argc, char **argv) {
               << "\n +           TUP           + "
               << "\n +  Low Voltage Condition  + "
               << "\n +        Watchdog         + "
-              << "\n +++++++++++++++++++++++++++ ";
+              << "\n +++++++++++++++++++++++++++ " << std::endl;
 
     using Message = std::string;
     auto const port = "/dev/ttyS2";
     auto constexpr boudrate = 115200;
-    auto constexpr timeout = std::chrono::milliseconds(500);
+    auto constexpr timeout = std::chrono::milliseconds(300);
     auto constexpr ret_size = 548;
 
     try {
@@ -32,10 +33,67 @@ int main(int argc, char **argv) {
             sc.send_recv(sc_msg_send, sc_msg_recv);
             // std::cout << std::endl << "[" << sc_msg_recv << "]\n";
             return sc_msg_recv;
-        }
+        };
 
-        auto const response = sc_send_recv("sn?");
-        std::cout << std::endl << "response:[" << response << "]";
+        sc_send_recv("\n");
+
+        while (true) {
+
+            std::cout << std::flush << "_";
+
+            auto response = sc_send_recv("sddc?");
+
+            // most likely case, all normal
+            if (response.find("Disable") != std::string::npos) {
+                std::cout << ".";
+                std::this_thread::sleep_for( std::chrono::milliseconds(500) );
+                continue;
+            }
+
+            // something else..
+
+            try {
+
+                // check if format is correct
+                std::string const title = "SDDC = ";
+                auto const title_index = response.find(title);
+
+                if (title_index == std::string::npos) {
+                    std::cerr << std::endl << "!bad response!";
+                    // don't throw
+                    continue;
+                }
+
+                // title was found
+                auto const value_index = title_index + title.size();
+                auto const value_and_extra = response.substr(value_index);
+                std::istringstream iss;
+                iss.str(value_and_extra);
+                std::string value_string;
+                std::getline(iss, value_string);
+
+                if ( value_string.empty() ) {
+                    std::cerr << std::endl << "!bad value_string!";
+                    // don't throw
+                    continue;
+                }
+
+                // format looks good
+
+                // since Disable has not been found, value is the time to shutdown
+                auto const value = std::stoi(value_string);
+                std::cout << std::endl << "tup will shutdown! | time:" << value;
+                std::cout << std::endl;
+                std::cout << std::endl << "computer will shutdown!" << std::endl;
+                std::this_thread::sleep_for( std::chrono::seconds(1) );
+                std::cout << std::endl << "bye!" << std::endl;
+                std::system("sudo shutdown now");
+                break;
+
+            } catch (...) {
+                std::cerr << "could not find value in string";
+            }
+        }
 
     } catch (std::exception & e) {
 
--- code/connection/test/test_serial.cpp
+++ code/connection/test/test_serial.cpp
@@ -21,7 +21,7 @@ TEST_F(FixtureConnection, connection_setup) {
 }
 
 // -------------------------------------------------------------------------- //
-TEST_F(FixtureConnection, serial_connection) {
+TEST_F(FixtureConnection, list_serial_ports) {
 
     std::string const serial_port = "/dev/ttyS1";
     auto constexpr baud_rate = 9600;
@@ -49,6 +49,34 @@ TEST_F(FixtureConnection, serial_connection) {
     // ss << msg;
     std::cout << ss.str();
 }
+
+// -------------------------------------------------------------------------- //
+TEST_F(FixtureConnection, serial_connection) {
+
+    std::string const serial_port = "/dev/ttyS2";
+    auto constexpr baud_rate = 115200;
+    auto constexpr timeout = std::chrono::milliseconds(500);
+
+    mzn::SerialConnection sc(serial_port, baud_rate, timeout);
+
+    using Message = std::string;
+    auto constexpr ret_size = 548;
+
+    try {
+
+        sc.setup_connection();
+        Message sc_msg_send = "sn?\r";
+        Message sc_msg_recv = "";
+        sc_msg_recv.resize(ret_size);
+        sc.send_recv(sc_msg_send, sc_msg_recv);
+        std::cout << std::endl << "[" << sc_msg_recv << "]\n";
+
+    } catch(std::exception const & e) {
+        std::cerr << std::endl << "caught error @ connection test\n";
+        throw mzn::WarningException("E300Server", "run", e.what() );
+    }
+}
+
 
 // -------------------------------------------------------------------------- //
 int main(int argc, char **argv) {
